487b07b15edcecade76f966cc1073a23b1af0a56
Artem Malyshev <proofit404@gmail.com>
2019-07-30 21:27:35 +0300

    wip

--- server/config/settings.py
+++ server/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'barbershop',
+        'USER': 'server',
+        'PASSWORD': 'top_secret',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
