44d930b0492363a772c7d6a0aabf3dec55e13b9f
Artem Malyshev <proofit404@gmail.com>
2019-07-31 20:53:29 +0300

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Button, Container, Menu } from 'semantic-ui-react';
 
 function App() {
-  return <Header as='h1'>Hello</Header>
+  return (
+    <Container>
+      <Menu pointing secondary>
+        <Menu.Item as='a' active>Home</Menu.Item>
+        <Menu.Item as='a'>Work</Menu.Item>
+        <Menu.Item as='a'>Company</Menu.Item>
+        <Menu.Item as='a'>Careers</Menu.Item>
+        <Menu.Item position='right'>
+          <Button as='a'>
+            Log in
+          </Button>
+          <Button as='a' primary style={{ marginLeft: '0.5em' }}>
+            Sign Up
+          </Button>
+        </Menu.Item>
+      </Menu>
+    </Container>
+  );
 }
 
 export default App;
