c73a28bd5d641363225ae975daeba7c73eb3a246
Artem Malyshev <proofit404@gmail.com>
2019-08-02 20:07:51 +0300

    wip

--- start.sh
+++ start.sh
@@ -6,4 +6,16 @@ do
     sleep 2
 done
 
+SCRIPT_BIN="$(dirname $(readlink -f $0))"
+
+if [ ! -d $SCRIPT_BIN/server/.venv ]
+then
+    docker-compose -p workshop run --rm server poetry install
+fi
+
+if [ ! -d $SCRIPT_BIN/client/node_modules ]
+then
+    docker-compose -p workshop run --rm client npm install
+fi
+
 docker-compose -p workshop run --rm server poetry run python manage.py migrate
