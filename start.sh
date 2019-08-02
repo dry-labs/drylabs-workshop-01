d3c4d11a6ca1f78ecda98b637e0c6ed4789d9702
Artem Malyshev <proofit404@gmail.com>
2019-08-02 20:52:11 +0300

    wip

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-for SERVICE in postgres server client nginx
-do
-    docker-compose -p workshop up -d $SERVICE
-    sleep 2
-done
-
 SCRIPT_BIN="$(dirname $(readlink -f $0))"
 
 if [ ! -d $SCRIPT_BIN/server/.venv ]
@@ -17,5 +11,11 @@ if [ ! -d $SCRIPT_BIN/client/node_modules ]
 then
     docker-compose -p workshop run --rm client npm install
 fi
+
+for SERVICE in postgres server client nginx
+do
+    docker-compose -p workshop up -d $SERVICE
+    sleep 2
+done
 
 docker-compose -p workshop run --rm server poetry run python manage.py migrate
